972df053b1e92beca1cd58bc5110e06b413c785b
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-16 15:43:09 +0200

    [UPD] added the h1 content from vue

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // DESTRUTTURO LE COMPONENT DELL'APP 
-const { createApp } = vue;
+const { createApp } = Vue;
 // DEFINISCO LA STRUTTURA DELL'APP
 createApp({
     data() {
         return {
-            
+            // AGGIUNGO IL CONTENUTO DA POI INSERIRE NEL DOM
+            message: "Hello Vue"
         }
     }
 }).mount('#app');
