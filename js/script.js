dfb094f56e2344d1f2fa38f70e8e9493969bdbf5
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-16 15:34:13 +0200

    [UPD] added the basic structure of the vue app

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // DESTRUTTURO LE COMPONENT DELL'APP 
 const { createApp } = vue;
-
+// DEFINISCO LA STRUTTURA DELL'APP
+createApp({
+    data() {
+        return {
+            
+        }
+    }
+}).mount('#app');
